338dfa9ebfaf7edcee98bfcf2c7e160953e37ed7
april1703 <gauthreauxa@gmail.com>
2026-02-03 17:03:07 -0600

    keypadupdate

---
+++ Escape-the-Cyberspace/Assets/Keypad/Scripts/KeypadButtonXR.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class KeypadButtonXRHook : MonoBehaviour
+{
+    [SerializeField] private NavKeypad.KeypadButton keypadButton;
+    [SerializeField] private UnityEngine.XR.Interaction.Toolkit.Interactables.XRBaseInteractable interactable;
+
+    private void Reset()
+    {
+        keypadButton = GetComponent<NavKeypad.KeypadButton>();
+        interactable = GetComponent<UnityEngine.XR.Interaction.Toolkit.Interactables.XRBaseInteractable>();
+    }
+
+    private void OnEnable()
+    {
+        if (!interactable) interactable = GetComponent<UnityEngine.XR.Interaction.Toolkit.Interactables.XRBaseInteractable>();
+        interactable.selectEntered.AddListener(OnSelectEntered);
+    }
+
+    private void OnDisable()
+    {
+        interactable.selectEntered.RemoveListener(OnSelectEntered);
+    }
+
+    private void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        keypadButton.PressButton();
+    }
+}
