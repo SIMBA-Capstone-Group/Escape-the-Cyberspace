09c2d2d0e93a9b21b45629c049cca9df3deedafc
Bella Breaux <127979478+bella-breaux@users.noreply.github.com>
2026-02-24 00:54:57 -0600

    Update script

--- Escape-the-Cyberspace/Assets/Scripts/LoginManager.cs
+++ Escape-the-Cyberspace/Assets/Scripts/LoginManager.cs
@@ -4,45 +4,39 @@ using UnityEngine.EventSystems;
 
 public class LoginManager : MonoBehaviour, IPointerClickHandler
 {
-    [Header("UI Objects")]
-    public GameObject visualKeyboard;
-    public Text passwordInputField;  // Drag the 'Pass' text object from Monitor 2 here
-    public GameObject atbashCanvas;   // Drag the hidden Atbash Canvas here
-
-    [Header("Security")]
+    public GameObject keyboardUI;
+    public InputField passwordInputField;
+    public GameObject atbashCanvas;
     public string correctPassword = "BadPassword123";
 
-    // 1. This triggers when the VR Raycast clicks the Monitor Screen
     public void OnPointerClick(PointerEventData eventData)
     {
-        if (visualKeyboard != null)
+        if (keyboardUI != null)
         {
-            visualKeyboard.SetActive(true);
-            Debug.Log("Monitor Clicked: Showing Keyboard");
+            keyboardUI.SetActive(true);
         }
     }
 
-    // 2. This is called by the 'Enter' button on the Visual Keyboard
     public void ValidatePassword()
     {
         if (passwordInputField.text == correctPassword)
         {
-            UnlockAtbashPuzzle();
-        }
-        else
-        {
-            Debug.Log("Invalid Password: " + passwordInputField.text);
+            if (keyboardUI != null)
+            {
+                keyboardUI.SetActive(false);
+            }
+            if (atbashCanvas != null)
+            {
+                atbashCanvas.SetActive(true);
+            }
         }
     }
 
-    private void UnlockAtbashPuzzle()
+    public void ClearPassword()
     {
-        // Hide keyboard and show the Atbash PNG
-        if (visualKeyboard != null) visualKeyboard.SetActive(false);
-
-        if (atbashCanvas != null)
+        if (passwordInputField != null)
         {
-            atbashCanvas.SetActive(true);
+            passwordInputField.text = "";
         }
     }
 }
