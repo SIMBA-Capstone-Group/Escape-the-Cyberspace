6ce58a8a85e51c3e0f1fc93a1a1b433282757bef
SadieAnn <127803877+SadieStaar@users.noreply.github.com>
2026-04-28 14:36:38 -0500

    Add admin screen + logic

--- Escape-the-Cyberspace/Assets/Scripts/LoginManager.cs
+++ Escape-the-Cyberspace/Assets/Scripts/LoginManager.cs
@@ -11,6 +11,8 @@ public class LoginManager : MonoBehaviour, IPointerClickHandler
     public GameObject[] poweredOnScreens;
     public GameObject loginScreen;
 
+    public AdminLoginManager admin;
+
     [Header("Password Setup")]
     [Tooltip("Drag the TextMeshPro object of the sticky note you want this computer to use.")]
     public TMP_Text linkedStickyNote; // <-- Added this slot to hold the sticky note
@@ -58,6 +60,10 @@ correctPassword = linkedStickyNote.text.Replace("\u200B", "").Trim();        }
             if (KeyboardListener != null)
             {
                 KeyboardListener.NoMoreUIPlease();
+            }
+            if (admin != null)
+            {
+                admin.isLoggedIn = true;
             }
         }
         else if (onIncorrect != null)
