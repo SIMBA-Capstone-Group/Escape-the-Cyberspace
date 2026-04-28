1c1a6673dc7a30ced5b3eae413ad095eb6582ab4
SadieAnn <127803877+SadieStaar@users.noreply.github.com>
2026-04-27 20:15:03 -0500

    Remove infinite loop

--- Escape-the-Cyberspace/Assets/Scripts/DynamicCPT.cs
+++ Escape-the-Cyberspace/Assets/Scripts/DynamicCPT.cs
@@ -1,6 +1,5 @@
-using System.Linq;
+using System.Collections.Generic;
 using TMPro;
-using UnityEditor.Rendering;
 using UnityEngine;
 
 public class DynamicCPT : MonoBehaviour
@@ -8,26 +7,23 @@ public class DynamicCPT : MonoBehaviour
     public TextMeshProUGUI[] employeeSubnet;
     public TextMeshProUGUI[] adminSubnet;
     public TextMeshProUGUI[] utilitiesSubnet;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
-        string [] mask1 = GetRandomIP(employeeSubnet.Length, "10.0.3.");
-
-        for(int i = 0; i > employeeSubnet.Length; i++)
+        string[] mask1 = GetRandomIP(employeeSubnet.Length, "10.0.3.");
+        for (int i = 0; i < employeeSubnet.Length; i++)
         {
             employeeSubnet[i].text = mask1[i];
         }
 
         string[] mask2 = GetRandomIP(adminSubnet.Length, "10.0.1.");
-
-        for(int i = 0; i > adminSubnet.Length; i++)
+        for (int i = 0; i < adminSubnet.Length; i++)
         {
             adminSubnet[i].text = mask2[i];
         }
 
         string[] mask3 = GetRandomIP(utilitiesSubnet.Length, "10.0.2.");
-
-        for(int i = 0; i > utilitiesSubnet.Length; i++)
+        for (int i = 0; i < utilitiesSubnet.Length; i++)
         {
             utilitiesSubnet[i].text = mask3[i];
         }
@@ -35,24 +31,20 @@ public class DynamicCPT : MonoBehaviour
 
     private string[] GetRandomIP(int length, string header)
     {
-        string[] ips = {};
-        int[] chosenMasks = {};
-        for(int i = 0; i < length; i++)
-        {
-            int temp = 0;
-            do
-            {
-                temp = Random.Range(2, 224);
-            }
-            while (!chosenMasks.Contains(temp));
-            ips.Append(header + (temp).ToString());
-        }
-        return ips;
-    }
+        List<string> ips = new List<string>();
+        HashSet<int> used = new HashSet<int>();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        while (ips.Count < length)
+        {
+            int temp = Random.Range(2, 224);
+
+            if (!used.Contains(temp))
+            {
+                used.Add(temp);
+                ips.Add(header + temp.ToString());
+            }
+        }
+
+        return ips.ToArray();
     }
 }
